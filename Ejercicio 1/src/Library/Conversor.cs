aa169246d3323d32c838a8655ea0de1ee5a8a561
brunoarnuti <brunoarnuti@gmail.com>
2020-09-01 20:22:00 -0300

    realizo los cambios

--- Ejercicio 1/src/Library/Conversor.cs
+++ Ejercicio 1/src/Library/Conversor.cs
@@ -4,7 +4,7 @@ namespace Expert_SRP
 {
     public static class Conversor 
     {
-        public static Double ConvertirAPesos(Double dinero, String moneda)//En mi opinion, seria mejor tener una clase que se encargue de las conversiones, ya que si quisieramos modificar algo de la forma en la que se hacen las conversiones hay que modificar la clase kiosco, cosa que no tiene mucho sentido. (SRP)
+        public static Double ConvertirAPesos(Double dinero, String moneda)
         {
             if (moneda.Equals("U$S")) 
             {
--- Ejercicio 1/src/Library/Kiosco.cs
+++ Ejercicio 1/src/Library/Kiosco.cs
@@ -10,6 +10,6 @@ namespace Expert_SRP
             //return pesos >= a.PrecioDulce + a.PrecioMasa; Lo ideal seria que la propia clase alfajor sume los precios, ya que es esa clase la que tiene la informacion idicada para hacerlo (Expert)
             return pesos >= a.GetPrecioTotal();
         }
-     
+        //En mi opinion, seria mejor tener una clase que se encargue de las conversiones, ya que si quisieramos modificar algo de la forma en la que se hacen las conversiones hay que modificar la clase kiosco, cosa que no tiene mucho sentido. (SRP)
     }
 }
