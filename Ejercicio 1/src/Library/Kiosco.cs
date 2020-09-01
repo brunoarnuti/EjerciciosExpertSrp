83a5cbe1b8053d2659879d8bc53f5f6cf65ece30
brunoarnuti <brunoarnuti@gmail.com>
2020-09-01 20:09:43 -0300

    agrego comentarios sobre lo que podria mejorar

--- Ejercicio 1/src/Library/Kiosco.cs
+++ Ejercicio 1/src/Library/Kiosco.cs
@@ -4,12 +4,12 @@ namespace Expert_SRP
 {
     public class Kiosco 
     {
-        public Boolean PuedeComprar(Alfajor a, Double dinero, String moneda) 
+        public Boolean PuedeComprar(Alfajor a, Double dinero, String moneda)
         {
             Double pesos = ConvertirAPesos(dinero, moneda);
-            return pesos >= a.PrecioDulce + a.PrecioMasa;
+            return pesos >= a.PrecioDulce + a.PrecioMasa;//Lo ideal seria que la propia clase alfajor sume los precios, ya que es esa clase la que tiene la informacion idicada para hacerlo (Expert)
         }
-        private Double ConvertirAPesos(Double dinero, String moneda)
+        private Double ConvertirAPesos(Double dinero, String moneda)//En mi opinion, seria mejor tener una clase que se encargue de las conversiones, ya que si quisieramos modificar algo de la forma en la que se hacen las conversiones hay que modificar la clase kiosco, cosa que no tiene mucho sentido. (SRP)
         {
             if (moneda.Equals("U$S")) 
             {
