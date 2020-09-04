a449f3c336c742faf8debca5e2fea3f0ca9d3de8
brunoarnuti <brunoarnuti@gmail.com>
2020-09-04 17:33:56 -0300

    agrego comentarios 2

--- Ejercicio 2/src/Library/Libro.cs
+++ Ejercicio 2/src/Library/Libro.cs
@@ -8,7 +8,7 @@ namespace SRP
         public string Titulo { get ; }
         public string Autor { get ; }
         public string Codigo { get ;  }
-        public string SectorBiblioteca { get ; set; }
+        public string SectorBiblioteca { get ; set; } //En el caso de que en un futuro se quiera modelar un libro electronico con esta misma clase, no tiene mucho sentido que una instancia de libro electronico tenga atributos tales como sectores de biblioteca
         public string EstanteBiblioteca { get ; set; }
 
         public Libro(String titulo, String autor, String codigo)
@@ -20,6 +20,7 @@ namespace SRP
 
         public void AlmacenarLibro(String sector, String estante)//No me parece que la clase libro sepa acerca la organizacion de la biblioteca, supongase que se quiera usar esta clase libro para modelar libros electronicos, ya no tendria sentido este metodo
                                                                  //por eso me parece correcto la creacion de otra clase llamada biblioteca
+                                                                 //Si bien tengo en cuenta lo que dije anteriormente, no se me ocurre una forma facil de extraer este metodo y re-estructurar las clases sin usar herencia
         {
             this.SectorBiblioteca = sector;
             this.EstanteBiblioteca = estante;
