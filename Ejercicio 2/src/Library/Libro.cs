709821ff311717e6ea8064c5327b3d90b7f0d121
brunoarnuti <brunoarnuti@gmail.com>
2020-09-04 18:50:52 -0300

    agrego comentarios

--- Ejercicio 2/src/Library/Libro.cs
+++ Ejercicio 2/src/Library/Libro.cs
@@ -18,9 +18,9 @@ namespace SRP
             this.Codigo = codigo;
         }
 
-        public void AlmacenarLibro(String sector, String estante)//No me parece que la clase libro sepa acerca la organizacion de la biblioteca, supongase que se quiera usar esta clase libro para modelar libros electronicos, ya no tendria sentido este metodo
-                                                                 //por eso me parece correcto la creacion de otra clase llamada biblioteca
-                                                                 //Si bien tengo en cuenta lo que dije anteriormente, no se me ocurre una forma facil de extraer este metodo y re-estructurar las clases sin usar herencia
+        public void AlmacenarLibro(String sector, String estante)//No me parece correcto que la clase libro sepa acerca la organizacion de la biblioteca, supongase que se quiera usar esta clase libro para modelar libros electronicos, ya no tendria sentido este metodo
+                                                                 //por eso me parece correcto la creacion de otra clase llamada biblioteca. Tambien cabe destacar el hecho de que si cambia la manera en la que se representa la bilbioteca, la clase Libro deberia cambiar, y si cambia la manera en la que se representan los Libros, tambien deberia cambiar, eso viola el SRP, hay mas de una razon de cambio.
+                                                                 //Si bien tengo en cuenta lo que dije anteriormente, no se me ocurre una forma facil ni beneficioso el extraer este metodo y re-estructurar las clases sin usar herencia
         {
             this.SectorBiblioteca = sector;
             this.EstanteBiblioteca = estante;
